3c72c941c497bbd37406c4ad6883a0c8703c4619
root <root@servidorUbuntu>
2024-04-23 15:38:13 +0000

    Mi segundo commit desde servidorUbuntu

--- microOrdenes/src/index.js
+++ microOrdenes/src/index.js
@@ -5,6 +5,6 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(ordenesController);
-app.listen(3007, () => {
-    console.log('Microservicio de ordenes con docker swarm escuchando en el puerto 3007');
+app.listen(3008, () => {
+    console.log('Microservicio de ordenes con docker swarm escuchando en el puerto 3008');
 });
